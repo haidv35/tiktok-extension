628785e1857291b011de626eb56eaa6576cc0709
cl0v3r <haiclover@cl0v3rs-MacBook-Pro.local>
2020-02-18 13:53:12 +0700

    First Commit

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+// changeColor.onclick = function(element) {
+// 	let color = element.target.value;
+// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// 	  chrome.tabs.executeScript(
+// 	      tabs[0].id,
+// 	      {code: 'document.body.style.backgroundColor = "' + color + '";'});
+// 	});
+// };
