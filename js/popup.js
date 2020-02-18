84cf0b9c03199d9e3f48e84d85f27effea5b9046
cl0v3r <haiclover@cl0v3rs-MacBook-Pro.local>
2020-02-18 22:34:13 +0700

    Update popup.html & popup.js

--- js/popup.js
+++ js/popup.js
@@ -4,11 +4,66 @@
 
 'use strict';
 
-// changeColor.onclick = function(element) {
-// 	let color = element.target.value;
-// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-// 	  chrome.tabs.executeScript(
-// 	      tabs[0].id,
-// 	      {code: 'document.body.style.backgroundColor = "' + color + '";'});
-// 	});
-// };
+document.addEventListener('DOMContentLoaded', function () {
+	var data,dataLength,current = 0;
+	function display(data){
+		let uploadDate,user;
+		document.getElementById("video").src = data['contentUrl'];
+		user = data['url'].split("/")[3];
+	    document.getElementById("name").innerHTML = user;
+// 	    uploadDate = new Date(data['uploadDate']);
+// 	    document.getElementById("uploadDate").innerHTML = ('0' + uploadDate.getDate()).slice(-2)  + "-" + ('0' + (uploadDate.getMonth()+1)).slice(-2) + "-" + uploadDate.getFullYear() + " " +
+// ('0' + uploadDate.getHours()).slice(-2) + ":" + ('0' + uploadDate.getMinutes()).slice(-2);
+	    document.getElementById("description").innerHTML = data['name'];
+	    document.getElementById("music").innerHTML = data['audio']['name'];
+	}
+	function getJsonTikTok(index){
+		if(index != 0){
+			display(data[index]);
+		}
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "https://www.tiktok.com/node/share/trending", true);
+		xhr.onreadystatechange = function() {
+		  if (xhr.readyState == 4) {
+		  	data = JSON.parse(xhr.responseText)['body']['itemList'];
+		  	dataLength = data.length;
+		    display(data[index]);
+		  }
+		}
+		xhr.send();
+	};
+	getJsonTikTok(0);
+
+	//Next to new video
+	var scrollStatus = {
+	    wheeling: false,
+	    functionCall: false
+	  };
+	var scrollTimer = false;
+	window.addEventListener('wheel', function (event) {
+		if (event.deltaY < 0)
+		{
+
+		}
+		else if (event.deltaY > 0)
+		{
+			scrollStatus.wheeling = true;
+		    if (!scrollStatus.functionCall) {
+		    	if(current < dataLength){
+		    		current += 1;
+		    		getJsonTikTok(current);
+		    	}
+		    	else{
+		    		getJsonTikTok(0);
+		    	}
+		      	scrollStatus.functionCall = true;
+		    }
+		    window.clearInterval(scrollTimer);
+		    scrollTimer = window.setTimeout(function() {
+		      scrollStatus.wheeling = false;
+		      scrollStatus.functionCall = false;
+		    }, 400);
+		}
+	});
+});
+
